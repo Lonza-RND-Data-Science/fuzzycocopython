e8646c43cb0d4e22e0d1a4212a6b530c25a37581
Arthur Babey <arthur.babey@heig-vd.ch>
2025-10-27 10:09:10 +0100

    wp

--- testresset.py
+++ testresset.py
@@ -1,14 +1,15 @@
-from fuzzycocopython import FuzzyCocoRegressor
 import numpy as np
+
+from fuzzycocopython import FuzzyCocoRegressor
 
 model = FuzzyCocoRegressor(nb_rules=5, random_state=42)
 X = np.random.rand(10, 3)
 y = np.random.rand(10)
 
-model.fit(X,y)
+model.fit(X, y)
 
 
-#print(model.rules_stat_activations(X))
+# print(model.rules_stat_activations(X))
 print("----")
 model.set_target_names("new_target")
 print(model.predict(X[:5]))
